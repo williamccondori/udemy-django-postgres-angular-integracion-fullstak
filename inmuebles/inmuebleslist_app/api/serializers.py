3df2e864d47d2adfe94450b35e5e70bda97cda30
William Condori Quispe <williamccodori@outlook.com>
2022-02-01 10:02:02 -0500

    Agregar metodos HTTP

--- inmuebles/inmuebleslist_app/api/serializers.py
+++ inmuebles/inmuebleslist_app/api/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 
+from inmuebleslist_app.models import Inmueble
+
 class InmuebleSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     direccion = serializers.CharField()
@@ -7,3 +9,15 @@ class InmuebleSerializer(serializers.Serializer):
     descripcion = serializers.CharField()
     imagen = serializers.CharField()
     active = serializers.BooleanField()
+    
+    def create(self, validated_data):
+        return Inmueble.objects.create(**validated_data)
+    
+    def update(self, instance, validated_data):
+        instance.direccion = validated_data.get('direccion', instance.direccion)
+        instance.pais = validated_data.get('pais', instance.pais)
+        instance.descripcion = validated_data.get('descripcion', instance.descripcion)
+        instance.imagen = validated_data.get('imagen', instance.imagen)
+        instance.active = validated_data.get('active', instance.active)
+        instance.save()
+        return instance
--- inmuebles/inmuebleslist_app/api/views.py
+++ inmuebles/inmuebleslist_app/api/views.py
@@ -3,14 +3,40 @@ from inmuebleslist_app.models import Inmueble
 from inmuebleslist_app.api.serializers import InmuebleSerializer
 from rest_framework.decorators import api_view
 
-@api_view(['GET'])
-def inmueble_list(request):
-    inmuebles = Inmueble.objects.all()
-    serializer = InmuebleSerializer(inmuebles, many=True)
-    return Response(serializer.data)
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
+def inmueble_list(request):
+    if request.method == 'GET':
+        inmuebles = Inmueble.objects.all()
+        serializer = InmuebleSerializer(inmuebles, many=True)
+        return Response(serializer.data)
+    
+    if request.method == 'POST':
+        de_serializer = InmuebleSerializer(data=request.data)
+        if de_serializer.is_valid():
+            de_serializer.save()
+            return Response(de_serializer.data, status=201)
+        else:
+            return Response(de_serializer.errors, status=400)
+        
+@api_view(['GET', 'PUT', 'DELETE'])
 def inmueble_detalle(request, pk):
-    inmueble = Inmueble.objects.get(pk=pk)
-    serializer = InmuebleSerializer(inmueble)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        inmueble = Inmueble.objects.get(pk=pk)
+        serializer = InmuebleSerializer(inmueble)
+        return Response(serializer.data)
+    
+    if request.method == 'PUT':
+        inmueble = Inmueble.objects.get(pk=pk)
+        de_serializer = InmuebleSerializer(inmueble, data=request.data)
+        if de_serializer.is_valid():
+            de_serializer.save()
+            return Response(de_serializer.data)
+        else:
+            return Response(de_serializer.errors, status=400)
+
+    if request.method == 'DELETE':
+        inmueble = Inmueble.objects.get(pk=pk)
+        inmueble.delete()
+        return Response(status=204)
+        
