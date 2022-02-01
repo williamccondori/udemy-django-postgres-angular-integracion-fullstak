cdd75ca5bb098a0fd842c5d4f6c6a8bfb3674d5a
William Condori Quispe <williamccodori@outlook.com>
2022-02-01 12:14:49 -0500

    Agregar relaciones entre tablas

--- inmuebles/inmuebleslist_app/models.py
+++ inmuebles/inmuebleslist_app/models.py
@@ -2,17 +2,6 @@ from django.db import models
 
 # Create your models here.
 
-class Edificacion(models.Model):
-    direccion = models.CharField(max_length=250)
-    pais = models.CharField(max_length=150)
-    descripcion = models.CharField(max_length=500)
-    imagen = models.CharField(max_length=900)
-    active = models.BooleanField(default=True)
-    create = models.DateTimeField(auto_now_add=True)
-    
-    def __str__(self) -> str:
-        return self.direccion
-
 class Empresa(models.Model):
     nombre = models.CharField(max_length=250)
     website = models.URLField(max_length=250)
@@ -20,3 +9,15 @@ class Empresa(models.Model):
     
     def __str__(self):
         return self.nombre
+
+class Edificacion(models.Model):
+    direccion = models.CharField(max_length=250)
+    pais = models.CharField(max_length=150)
+    descripcion = models.CharField(max_length=500)
+    imagen = models.CharField(max_length=900)
+    active = models.BooleanField(default=True)
+    empresa = models.ForeignKey(Empresa, on_delete=models.CASCADE, related_name='edificaciones')
+    created = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self) -> str:
+        return self.direccion
