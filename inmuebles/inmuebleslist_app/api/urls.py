f4d1657cd24142bdd27aa61cad184861f1704441
William Condori Quispe <williamccodori@outlook.com>
2022-02-01 10:30:47 -0500

    Agregar APIView

--- inmuebles/inmuebleslist_app/api/urls.py
+++ inmuebles/inmuebleslist_app/api/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from inmuebleslist_app.api.views import inmueble_list, inmueble_detalle
+# from inmuebleslist_app.api.views import inmueble_list, inmueble_detalle
+from inmuebleslist_app.api.views import InmuebleListAV, InmuebleDetalleAV
 
 urlpatterns = [
-    path('list/', inmueble_list, name='inmueble-list'),
-    path('<int:pk>', inmueble_detalle, name='inmueble-detalle'),
+    # path('list/', inmueble_list, name='inmueble-list'),
+    # path('<int:pk>', inmueble_detalle, name='inmueble-detalle'),
+    path('list/', InmuebleListAV.as_view(), name='inmueble-list'),
+    path('<int:pk>/', InmuebleDetalleAV.as_view(), name='inmueble-detalle'),
 ]
